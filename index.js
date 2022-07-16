c109dd96de18d502d03881cb486979370dd32029
Jesus Santiago <jesusdavid2607@gmail.com>
2022-07-16 16:29:34 -0500

    Use Express router, add page not found handler
    Split route handlers into different files
    Install and set up prettier

--- index.js
+++ index.js
@@ -1,40 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 const app = express();
 
-// Include body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/add-product', (req, res, next) => {
-  console.log('Add product middleware');
-  res.send(
-    `
-    <html>
-      <head>
-        <title>Add product</title>
-      </head>
-      <body>
-        <form action="/product" method="POST">
-          <input type="text" name="title"/>
-          <button type="submit">Add product</button>
-        </form>
-      </body>
-    </html>`.trim()
-  );
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
-// Filter by request method
-app.post('/product', (req, res, next) => {
-  console.log('Product middleware');
-  console.log(req.body);
-  // redirect to '/'
-  res.redirect('/');
-});
-
-app.use('/', (req, res, next) => {
-  console.log('Home middleware');
-  res.send('<h1>Hello from Express!</h1>');
+app.use((req, res) => {
+  res.status(404).send('<h1>Page not found</h1>');
 });
 
 app.listen(3000);
