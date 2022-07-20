02dfa47d684802bcd536d809c16f1dfc6dfed21e
Jesus Santiago <jesusdavid2607@gmail.com>
2022-07-20 17:24:00 -0500

    Add helper function to get root directory of project

--- routes/shop.js
+++ routes/shop.js
@@ -2,21 +2,17 @@ const path = require('path');
 
 const express = require('express');
 
+const rootDir = require('../util/path');
+
 const router = express.Router();
 
 router.get('/', (req, res) => {
   /**
-   * __dirname is a global variable exposed by node and it
-   * contains the absolute path to /routes folder, the
-   * folder in which the current file is located
-   *
-   * '..' is needed to go up one level access the sibling folder 'views'
-   *
-   * Using path.join is preferred as this function creates paths
-   * that work either on Windows, Mac or Linux systems
+   * The variable 'rootDir' contains the root directory of
+   * the node project
    *
    * */
-  res.sendFile(path.join(__dirname, '..', 'views', 'shop.html'));
+  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
 module.exports = router;
