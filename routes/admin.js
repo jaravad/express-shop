6ddb64627bc5f97dd1a8a4afe72cbac90c387b8a
Jesus Santiago <jesusdavid2607@gmail.com>
2022-08-02 20:59:43 -0500

    Implement reusable layout template with pug

--- routes/admin.js
+++ routes/admin.js
@@ -9,7 +9,10 @@ const router = express.Router();
 const products = [];
 
 router.get('/add-product', (req, res) => {
-  res.render('add-product', { pageTitle: 'Add product' });
+  res.render('add-product', {
+    pageTitle: 'Add product',
+    path: '/admin/add-product',
+  });
 });
 
 router.post('/add-product', (req, res) => {
--- routes/shop.js
+++ routes/shop.js
@@ -10,7 +10,7 @@ const adminData = require('./admin');
 
 router.get('/', (req, res) => {
   const products = adminData.products;
-  res.render('shop', { products, pageTitle: 'Shop' });
+  res.render('shop', { products, pageTitle: 'Shop', path: '/' });
 });
 
 module.exports = router;
