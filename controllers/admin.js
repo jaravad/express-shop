1d41a63cef477be6ef9d2c00339c65da61654671
Jesus Santiago <jesusdavid2607@gmail.com>
2022-08-07 00:26:39 -0500

    Refactor controllers, add new views
    - Add new navigation links

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,9 +1,14 @@
 const Product = require('../models/product');
 
-const products = [];
+exports.getAdminProducts = (req, res) => {
+  res.render('admin/products', {
+    pageTitle: 'Admin products',
+    path: '/admin/products',
+  });
+};
 
 exports.getAddProduct = (req, res) => {
-  res.render('add-product', {
+  res.render('admin/add-product', {
     pageTitle: 'Add product',
     path: '/admin/add-product',
   });
@@ -13,14 +18,4 @@ exports.postAddProduct = (req, res) => {
   const product = new Product(req.body.title);
   product.save();
   res.redirect('/');
-};
-
-exports.getProducts = (req, res) => {
-  Product.fetchAll((products) => {
-    res.render('shop', {
-      products,
-      pageTitle: 'Shop',
-      path: '/',
-    });
-  });
 };
---
+++ controllers/shop.js
@@ -0,0 +1,32 @@
+const Product = require('../models/product');
+
+exports.getProducts = (req, res) => {
+  Product.fetchAll((products) => {
+    res.render('shop/product-list', {
+      products,
+      pageTitle: 'Products',
+      path: '/products',
+    });
+  });
+};
+
+exports.getIndex = (req, res) => {
+  Product.fetchAll((products) => {
+    res.render('shop/index', {
+      products,
+      pageTitle: 'Shop',
+      path: '/',
+    });
+  });
+};
+
+exports.getCart = (req, res) => {
+  res.render('shop/cart', { pageTitle: 'Cart', path: '/cart' });
+};
+
+exports.getCheckout = (req, res) => {
+  res.render('shop/checkout', {
+    pageTitle: 'Checkout',
+    path: '/checkout',
+  });
+};
